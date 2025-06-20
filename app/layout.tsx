14b8ade34b7b844c0127ebd482af9bdb6a41e7db
Anurag Srivastava <anurag01@Anurags-MacBook-Air.local>
2025-06-20 21:43:53 +0530

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,9 +94,9 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: '/logo2.png',
-    shortcut: '/logo2.png',
-    apple: '/logo2.png',
+    icon: '/logo_transparent.png',
+    shortcut: '/logo_transparent.png',
+    apple: '/logo_transparent.png',
   },
 }
 
